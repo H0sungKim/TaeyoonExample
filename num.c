0efb7b3402edaa7ccd183767fb0e095f9d18980d
hyongak516 <hyongak516@mail.hongik.ac.kr>
2022-03-30 12:21:51 +0900

    add numbergame

---
+++ num.c
@@ -0,0 +1,37 @@
+/** 
+ * Copyright (c) 2022 by Hosung.Kim <hyongak516@mail.hongik.ac.kr>
+ * ==============
+ * Hosung.Kim
+ * 2022.03.30
+ * --------------
+ * 한태윤 if문 예제
+ * ==============
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+    int randomNum = rand() % 100 + 1;
+    int count = 0;
+    int userNum;
+
+    printf("1~100 숫자 맞추기 게임입니다. 숫자를 입력하세요.\n=> ");
+    while (1) {
+        scanf("%d", &userNum);
+        if (randomNum > userNum) {
+            printf("%d보다 큽니다. ", userNum);
+        } else if (randomNum < userNum) {
+            printf("%d보다 작습니다. ", userNum);
+        } else {
+            printf("정답입니다. %d번만에 맞췄습니다.\n", count+1);
+            break;
+        }
+        count++;
+        printf("%d번 시도했습니다.\n=> ", count);
+    }
+    return 0;
+}
