58d69b19ea0fff35412c13ba5d3e83f1b32b30d5
hyongak516 <hyongak516@mail.hongik.ac.kr>
2022-04-01 15:42:33 +0900

    string add

---
+++ string.c
@@ -0,0 +1,22 @@
+/** 
+ * Copyright (c) 2022 by Hosung.Kim <hyongak516@mail.hongik.ac.kr>
+ * ==============
+ * Hosung.Kim
+ * 2022.03.30
+ * --------------
+ * 한태윤 string 예제
+ * ==============
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    // char inputStr[10];
+    char* inputStr = "Hosung.Kim";
+
+    // scanf("%s", inputStr);
+    printf("%s", inputStr);
+
+    return 0;
+}
